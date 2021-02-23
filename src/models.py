1cc051e780ed71532a69a8b0ada26e11efa047a0
Judycampk <judhycampk71@outlook.com>
2021-02-23 23:59:43 +0000

    diagram

--- src/models.py
+++ src/models.py
@@ -8,23 +8,70 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    url = Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250),nullable=False)
+
+class People_Details(Base):
+    __tablename__ = 'people_details'
+    id = Column(Integer, primary_key=True)
+    height = Column(Integer,nullable=False)
+    mass = Column(Integer,nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    birth_year = Column(Integer, nullable=False)
+    gender = Column(String(250), nullable=False)
+    created = Column(Integer, nullable=False)
+    edited = Column(Integer, nullable=False)
+    name = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'))
+    People = relationship(People)
+
+class Planets_Details(Base):
+    __tablename__ = 'planets_details'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer,nullable=False)
+    rotation_period = Column(Integer,nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(Integer, nullable=False)
+    created = Column(Integer, nullable=False)
+    edited = Column(Integer, nullable=False)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    Planets_id = Column(Integer, ForeignKey('planets.id'))
+    Planets = relationship(Planets)
+
+class User (Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    photo = Column(String,nullable=False) 
+
+class Favourites (Base):
+    __tablename__ = 'favourites'
+    id = Column(Integer, primary_key=True)
+    people = Column(Integer, ForeignKey('people.id'))
+    planets = Column(Integer, ForeignKey('planets.id'))
+    Favourites_id = Column(Integer, ForeignKey('user.id'))
+    Favourites = relationship(User)
 
     def to_dict(self):
         return {}
